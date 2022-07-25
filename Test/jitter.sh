bc78905db0c91cd38655ee419a55f7c5b63e8a8b
Nima Afraz <nima.afraz@ucd.ie>
2022-07-25 17:49:31 +0000

    Add new file

---
+++ Test/jitter.sh
@@ -0,0 +1,2 @@
+
+cat out.tr | grep " 2 3 cbr " | grep ^r | column 1 10 | awk '{dif = $2 - old2; if(dif==0) dif = 1; if(dif > 0) {printf("%d\t%f\n", $2, ($1 - old1) / dif); old1 = $1; old2 = $2}}' > jitter.txt
