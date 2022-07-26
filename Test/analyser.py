fb1bcd1cb63efdc920cd4c22b73253825b7cac97
Nima Afraz <nima.afraz@ucd.ie>
2022-07-26 06:05:38 +0000

    Add new file

---
+++ Test/analyser.py
@@ -0,0 +1,17 @@
+import  traceanalyzer as tr
+import matplotlib.pyplot as plt
+
+#end-to-end delay
+eedelay1=tr.Eedelay('out1.tr','33')
+eedelay2=tr.Eedelay('out2.tr','33')
+eedelay2.sample()#eedelay2.sample(1.5) for sampling with step=1.5
+eedelay1.plot()
+eedelay2.plot()
+plt.show()
+#getting data
+time=eedelay2.time_sample
+eedelay=eedelay2.eedelay_sample
+idx=0
+for instant in time:
+    print(instant,' ',eedelay[idx]) 
+    idx+=1
