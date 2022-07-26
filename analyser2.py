f5cb9be900eb127a6fe24855ce3ad296aaf12793
Nima Afraz <nima.afraz@ucd.ie>
2022-07-26 09:42:29 +0000

    Update analyser2.py

--- analyser2.py
+++ analyser2.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 #end-to-end delay
 eedelay1=tr.Eedelay('renoTrace.tr','3')
 eedelay2=tr.Eedelay('vegasTrace.tr','3')
+eedelay3=tr.Eedelay('cubicTrace.tr','3')
+eedelay4=tr.Eedelay('yeahTrace.tr','3')
+
 # eedelay1=tr.Eedelay('renoTrace.tr','0')
 # eedelay1=tr.Eedelay('renoTrace.tr','0')
 # eedelay2.sample()#eedelay2.sample(1.5) for sampling with step=1.5
 eedelay1.plot()
 eedelay2.plot()
+eedelay3.plot()
+eedelay4.plot()
 plt.show()
 #getting data
 time=eedelay2.time_sample
